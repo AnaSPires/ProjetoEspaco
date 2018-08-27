dba5e599b7a3842b942c334f5fa6d49e98a09366
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2018-08-27 09:41:43 -0300

    Update MER.sql

--- MER.sql
+++ MER.sql
@@ -11,3 +11,15 @@ dataAcesso datetime not null,
 horaAcesso datetime not null,
 constraint fkUsuario foreign key(codUsuario) references Usuario(CodUsuario)
 )
+   
+
+create table Respostas(
+codResposta ntext primary key,
+pontuacao int not null
+)
+                 
+create table Quiz(
+codPergunta ntext primary key,
+codResposta ntext not null,
+constraint fkRespostas foreign key(codRespostas) references Respostas(codRespostas)
+)
