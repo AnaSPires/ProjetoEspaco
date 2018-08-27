4103f5a903221ec67bb1b452c413103143fc503f
AnaSPires <42739437+AnaSPires@users.noreply.github.com>
2018-08-27 09:46:40 -0300

    Update MER.sql

--- MER.sql
+++ MER.sql
@@ -27,3 +27,20 @@ constraint fkRespostas foreign key(codRespostas) references Respostas(codRespost
 create table Arquivos(
 
 )
+create table SistemaSolar(
+codSistema int primary key,
+descricao ntext)
+
+create table Elementos(
+codAcesso int not null,
+nome varchar(30) not null,
+descricao ntext,
+tamanho varchar(15),
+massa varchar(15),
+distancia varchar(15),
+periodoOrbital varchar(15),
+periodoRotacao varchar(15),
+gravidade varchar(15),
+temperatura varchar(15),
+composicao varchar(30),
+estrutura varchar(15))
