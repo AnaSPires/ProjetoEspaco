5187b63e5fc1ae5a7521c7b33dc21fe89734fbd5
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2018-10-15 10:20:37 -0300

    15/10/2018

---
+++ Node.js/appSQL-alterado.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const porta = 3000; //porta padrão
+const sql = require('mssql');
+const conexaoStr = "Server=regulus;Database=PR118201;User Id=PR118201;Password=PR118201;";
+
+//conexao com BD
+sql.connect(conexaoStr)
+   .then(conexao => global.conexao = conexao)
+   .catch(erro => console.log(erro));
+
+// configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+/////////////////////////////////
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+/////////////////////////////////
+
+//definindo as rotas
+const rota = express.Router();
+rota.get('/', (requisicao, resposta) => resposta.json({ mensagem: 'Funcionando!'}));
+app.use('/', rota);
+
+//inicia servidor
+app.listen(porta);
+console.log('API Funcionando!');
+
+function execSQL(sql, resposta) {
+	global.conexao.request()
+		.query(sql)
+		.then(resultado => resposta.json(resultado.recordset))
+		.catch(erro => resposta.json(erro));
+}
+
+rota.get('/elementos', (requisicao, resposta) =>{
+execSQL('SELECT * FROM Elementos', resposta);
+})
+
+//o simbolo ? indica que id na rota abaixo é opcional
+rota.get('/elementos/:id?', (requisicao, resposta) => {
+let filtro = '';
+if (requisicao.params.id)
+filtro = ' WHERE codAcesso=' + parseInt(requisicao.params.id);
+execSQL('SELECT * from Elementos' + filtro, resposta);
+})
