3aaec2c5a22181a52037223a4f574da84941d46b
AnaSPires <42739437+AnaSPires@users.noreply.github.com>
2018-10-22 10:02:13 -0200

    22/10/2018
    teste medonho

--- MER.sql
+++ MER.sql
@@ -2,7 +2,11 @@ create table Usuario(
 CodUsuario int primary key,
 Nome varchar(30),
 dataNascimento datetime,
-senha  varbinary(10))
+senha  varchar(10))
+
+drop table Usuario
+select * from Usuario
+insert into Usuario values( 1, 'Amanda', '03/05/2009',25632514)
 
 create table Acesso(
 codAcesso int primary key,
@@ -12,37 +16,64 @@ horaAcesso datetime not null,
 constraint fkUsuario foreign key(codUsuario) references Usuario(CodUsuario)
 )
    
+select * from Acesso
 
-create table Respostas(
-codResposta ntext primary key,
-pontuacao int not null
+create table Perguntas(
+codPergunta int identity(1, 1) primary key not null, 
+enunciado ntext not null,
+alternativaA ntext not null,
+alternativaB ntext not null,
+alternativaC ntext not null,
+alternativaD ntext not null,
+correta char not null
 )
-                 
-create table Quiz(
-codPergunta ntext primary key,
-codResposta ntext not null,
-constraint fkRespostas foreign key(codRespostas) references Respostas(codRespostas)
-)
+
+select * from Perguntas
+
+insert into Perguntas values('Qual o maior planeta do sistema solar?', 'Plutão', 'Júpiter', 'Saturno', 'Terra', 'b');
+insert into Perguntas values('Qual o menor planeta do sistema solar?', 'Mercúrio', 'Netuno', 'Marte', 'Terra', 'a');
+insert into Perguntas values('Qual o planeta mais legal do sistema solar?', 'Plutão', 'Júpiter', 'Saturno', 'Terra', 'd');
 
 create table Arquivos(
 imagens varchar(30),
 sons varchar(30),
 videos varchar(30)
 )
+
+select * from Arquivos
+
 create table SistemaSolar(
 codSistema int primary key,
 descricao ntext)
 
+select * from SistemaSolar
+
 create table Elementos(
 codAcesso int not null,
 nome varchar(30) not null,
-descricao ntext,
-tamanho varchar(15),
-massa varchar(15),
+descricao varchar(30),
+tamanho varchar(45),
+massa varchar(45),
 distancia varchar(15),
 periodoOrbital varchar(15),
 periodoRotacao varchar(15),
 gravidade varchar(15),
 temperatura varchar(15),
 composicao varchar(30),
-estrutura varchar(15))
+estrutura varchar(15)
+)
+ 
+select * from Elementos
+
+insert into Elementos values(1, 'Mercúrio', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(2, 'Vênus', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(3, 'Terra', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(4, 'Marte', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(5, 'Júpiter', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(6, 'Saturno', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(7, 'Urano', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(8, 'Netuno', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(9, 'Plutão', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+insert into Elementos values(10, 'Sol', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
+
+
