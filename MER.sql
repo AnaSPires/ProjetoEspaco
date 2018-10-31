0ceb72b4e4a1f65be9012385701c31b3fc8cb9f1
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2018-10-31 09:47:29 -0300

    31/10/2018

--- MER.sql
+++ MER.sql
@@ -87,6 +87,30 @@ temperatura varchar(15),
 composicao varchar(30),
 estrutura varchar(15)
 )
+
+alter table Elementos
+alter column descricao ntext
+
+alter table Elementos
+alter column distancia ntext
+
+alter table Elementos
+alter column periodoRotacao ntext
+
+alter table Elementos
+alter column periodoOrbital ntext
+
+alter table Elementos
+alter column gravidade ntext
+
+alter table Elementos
+alter column composicao ntext
+
+alter table Elementos
+alter column estrutura ntext
+
+alter table Elementos
+alter column temperatura ntext
  
 select * from Elementos
 
@@ -101,4 +125,126 @@ insert into Elementos values(8, 'Netuno', 'Mais perto do sol', 'Mercúrio', 'Meno
 insert into Elementos values(9, 'Plutão', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
 insert into Elementos values(10, 'Sol', 'Mais perto do sol', 'Mercúrio', 'Menor do sistema', 'fds', 'ds', 'fd', 'fd','fd','s','dd')
 
+---Marte---
+update Elementos set descricao = 'É o quarto planeta a partir do Sol e o segundo menor planeta do Sistem Solar. Seu nome foi uma homenagem ao deus romano da guerra, é também conhecido como "Planeta Vermelho", devido à grande quantidade de óxido de ferro(hematita) em sua superfície, que lhe dá uma aparência avermelhada. 
+ É um Planeta Rochoso, que tem as estações do ano similares às da Terra. Tem 2 Luas conhecidas: Fobos e Deimos, e ao contrário de outos planetas, pode ser visto à olho nu da Terra. 
+ Possui água corrente durante os meses mais quentes, vinda do gelo em seus polos. De sua massa, é estimado que entre 1,5% e 3% sejam de água.' where codAcesso = 4
+update Elementos set tamanho = '6.779 km' where codAcesso = 4
+update Elementos set massa = '6,39 X 10^23 kg' where codAcesso = 4
+update Elementos set distancia = 'Do Sol: 230 000 000 km (1,5 UA) - Da Terra: =~ 57,6 milhões de quilômetros' where codAcesso = 4
+update Elementos set periodoOrbital = '687 dias' where codAcesso = 4
+update Elementos set periodoRotacao = '24 horas e 37 minutos' where codAcesso = 4
+update Elementos set gravidade = '(Equatorial) -- 3.711 m/s² g' where codAcesso = 4
+update Elementos set temperatura = '-63°C' where codAcesso = 4
+update Elementos set composicao = '95% de dióxido de carbono, 3% nitrogênio, 1.6% argônio e traços de oxigênio, água, e metano' where codAcesso = 4
+update Elementos set estrutura = '' where codAcesso = 4
+---Mercúrio---
+update Elementos set descricao = '' where codAcesso = 1
+update Elementos set tamanho = '4.879 km' where codAcesso = 1
+update Elementos set massa = '200,59 u ± 0,02 u ' where codAcesso = 1
+update Elementos set distancia = 'Do Sol: 57.910.000 km Da Terra: ' where codAcesso = 1
+update Elementos set periodoOrbital = '88 dias' where codAcesso = 1
+update Elementos set periodoRotacao = '58,646 dias (?1 407,5?) horas' where codAcesso = 1
+update Elementos set gravidade = '3,7 m/s²' where codAcesso = 1
+update Elementos set temperatura = '169,35 °C' where codAcesso = 1
+update Elementos set composicao = '' where codAcesso = 1
+update Elementos set estrutura = '' where codAcesso = 1
+---Vênus--
+update Elementos set descricao = '' where codAcesso = 2
+update Elementos set tamanho = '12.104 km' where codAcesso = 2
+update Elementos set massa = '4,8×1020 kg ' where codAcesso = 2
+update Elementos set distancia = 'Do Sol: 108.200.000 km  Da Terra: ' where codAcesso = 2
+update Elementos set periodoOrbital = '225 dias' where codAcesso = 2
+update Elementos set periodoRotacao = '?116d 18h 0m' where codAcesso = 2
+update Elementos set gravidade = '8,87 m/s²' where codAcesso = 2
+update Elementos set temperatura = '461 ºC' where codAcesso = 2
+update Elementos set composicao = '' where codAcesso = 2
+update Elementos set estrutura = '' where codAcesso = 2
 
+---Terra--
+update Elementos set descricao = '' where codAcesso = 3
+update Elementos set tamanho = '12.742 km' where codAcesso = 3
+update Elementos set massa = '5.9722 × 1024 kg ' where codAcesso = 3
+update Elementos set distancia = 'Do Sol: 149.600.000 km  Da Terra: ' where codAcesso = 3
+update Elementos set periodoOrbital = '365 dias' where codAcesso = 3
+update Elementos set periodoRotacao = '?24h' where codAcesso = 3
+update Elementos set gravidade = '9,8m/s²' where codAcesso = 3
+update Elementos set temperatura = '14°C' where codAcesso = 3
+update Elementos set composicao = '' where codAcesso = 3
+update Elementos set estrutura = '' where codAcesso = 3
+
+---Jupiter--
+update Elementos set descricao = '' where codAcesso = 5
+update Elementos set tamanho = '139.822 km' where codAcesso = 5
+update Elementos set massa = '1,8986 × 1027 kg' where codAcesso = 5
+update Elementos set distancia = 'Do Sol: 778.500.000 km Da Terra: ' where codAcesso = 5
+update Elementos set periodoOrbital = '12 anos' where codAcesso = 5
+update Elementos set periodoRotacao = '?0d 9h 56m' where codAcesso = 5
+update Elementos set gravidade = '24,79 m/s²' where codAcesso = 5
+update Elementos set temperatura = '-108 ºC' where codAcesso = 5
+update Elementos set composicao = '' where codAcesso = 5
+update Elementos set estrutura = '' where codAcesso = 5
+
+---Saturno--
+update Elementos set descricao = '' where codAcesso = 6
+update Elementos set tamanho = '116.464 km,' where codAcesso = 6
+update Elementos set massa = '5,6846 × 1026 kg' where codAcesso = 6
+update Elementos set distancia = 'Do Sol: 1,434 × 10^9 km  Da Terra: ' where codAcesso = 6
+update Elementos set periodoOrbital = '29 anos' where codAcesso = 6
+update Elementos set periodoRotacao = '?0d 10h 42m' where codAcesso = 6
+update Elementos set gravidade = '10,44 m/s²' where codAcesso = 6
+update Elementos set temperatura = '-139 ºC ' where codAcesso = 6
+update Elementos set composicao = '' where codAcesso = 6
+update Elementos set estrutura = '' where codAcesso = 6
+
+---Urano--
+update Elementos set descricao = '' where codAcesso = 7
+update Elementos set tamanho = '50.724 km' where codAcesso = 7
+update Elementos set massa = '238,02891 u ± 0,00003 u' where codAcesso = 7
+update Elementos set distancia = 'Do Sol: 2,871 × 10^9 km  Da Terra: ' where codAcesso = 7
+update Elementos set periodoOrbital = '84 anos' where codAcesso = 7
+update Elementos set periodoRotacao = '?0d 17h 14m' where codAcesso = 7
+update Elementos set gravidade = '8,87 m/s²' where codAcesso = 7
+update Elementos set temperatura = '–224 °C ' where codAcesso = 7
+update Elementos set composicao = '' where codAcesso = 7
+update Elementos set estrutura = '' where codAcesso = 7
+
+
+---Netuno--
+update Elementos set descricao = '' where codAcesso = 8
+update Elementos set tamanho = '49.244 km' where codAcesso = 8
+update Elementos set massa = '237.0482 u' where codAcesso = 8
+update Elementos set distancia = 'Do Sol: 4,495 × 10^9 km Da Terra: ' where codAcesso = 8
+update Elementos set periodoOrbital = '165 anos' where codAcesso = 8
+update Elementos set periodoRotacao = '?0d 16h 6m' where codAcesso = 8
+update Elementos set gravidade = '11,15 m/s²' where codAcesso = 8
+update Elementos set temperatura = '-210°C' where codAcesso = 8
+update Elementos set composicao = '' where codAcesso = 8
+update Elementos set estrutura = '' where codAcesso = 8
+
+
+---Plutão--
+update Elementos set descricao = '' where codAcesso = 9
+update Elementos set tamanho = '2.377 km' where codAcesso = 9
+update Elementos set massa = '?(1,305 ± 0,007) × 10^22 kg' where codAcesso = 9
+update Elementos set distancia = 'Do Sol: 32,9 UA Da Terra: ' where codAcesso = 9
+update Elementos set periodoOrbital = '248 anos' where codAcesso = 9
+update Elementos set periodoRotacao = '153 horas?' where codAcesso = 9
+update Elementos set gravidade = '0,62 m/s²' where codAcesso = 9
+update Elementos set temperatura = '-233ºC ' where codAcesso = 9
+update Elementos set composicao = '' where codAcesso = 9
+update Elementos set estrutura = '' where codAcesso = 9
+
+---Sol--
+update Elementos set descricao = '' where codAcesso = 10
+update Elementos set tamanho = '1.391.016 km ' where codAcesso = 10
+update Elementos set massa = '?333000*5.9722 × 1024 kg?' where codAcesso = 10
+update Elementos set distancia = ' Da Terra: 149.600.000 km' where codAcesso = 10
+update Elementos set periodoOrbital = '' where codAcesso = 10
+update Elementos set periodoRotacao = '27 dias no seu Equador e 32 dias nos seus pólos?' where codAcesso = 10
+update Elementos set gravidade = '274 m/s²' where codAcesso = 10
+update Elementos set temperatura = '6.000°C(parte externa) e 15.000.000° C(no núcleo)' where codAcesso = 10
+update Elementos set composicao = '' where codAcesso = 10
+update Elementos set estrutura = '' where codAcesso = 10
+
+select * from Elementos
